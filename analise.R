ef796e7d37fa80daa410c6c3a16a3561f840e473
Sergio Vinicius Serejo da Costa Filho <sergio.costa@arauco.com>
2019-09-13 16:47:31 -0300

    elaboração do arquivo .rmd

--- analise.R
+++ analise.R
@@ -47,53 +47,71 @@ AREA <- append(AREA,sum(as.numeric(as.character(dados[[i]][,which(colnames(dados
 MES[which(MES%in%seq(1,9,1))] <- paste0(0,MES[which(MES%in%seq(1,9,1))])
 
 dataset <- data.frame(ANO=ANO,MES=MES,AREA=AREA,
-                      MES_ANO=as.Date(paste(ANO,'-',MES,'-01', sep='')))
+                      `MES.ANO`=as.Date(paste(ANO,'-',MES,'-01', sep='')))
 
 library(ggplot2)
+library(plotly)
 
-ggplot(dataset,aes(x=MES_ANO,y=AREA))+
+ggplotly(ggplot(dataset,aes(x=`MES.ANO`,y=AREA))+
   geom_line()+
-  scale_x_date(expand = c(0.05,0.05), breaks = '6 months',
-               date_labels = '%b', minor_breaks = NULL)+
-  scale_y_continuous(expand = c(0,0), limits = c(0,1500),
+    scale_x_date(expand = c(0.05,0.05), breaks = '6 months',
+                 date_labels = '%b %Y', minor_breaks = NULL)+  scale_y_continuous(expand = c(0,0), limits = c(0,1500),
                      breaks = seq(0,1500,100))+
   xlab(NULL)+
-  ylab('Área (km²)')+
+  ylab('Área desmatada (km²)')+
+  ggtitle('Área desmatada na Amazônia Legal - Acumulado mensal')+
   theme_light()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
+  theme(axis.text.x = element_text(angle=45, hjust = 1),
+        plot.title = element_text(hjust=0.5)))
 
-
-ggplot(dataset,aes(x=MES_ANO,y=AREA))+
-  geom_line()+
-  scale_x_date(expand = c(0.05,0.05), breaks = '6 months',
-               date_labels = '%b', minor_breaks = NULL)+
-  scale_y_continuous(expand = c(0,0), limits = c(0,1500),
-                     breaks = seq(0,1500,100))+
-  xlab(NULL)+
-  ylab('Área (km²)')+
-  theme_light()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
-
-
-
-library(lubridate)
-dataset$SEMESTRE <- semester(dataset$MES_ANO)
-dataset$SEMESTRE_ANO <- semester(dataset$MES_ANO, with_year = TRUE)
-
-
-dataset$AREA_ACUM_1ano <- dataset$AREA
+dataset$`AREA ACUMULADA EM 1 ANO` <- NA
 for(i in 12:nrow(dataset)){
-    dataset$AREA_ACUM_1ano[i] <- sum(dataset$AREA[i-c(0:11)])
-}
+    dataset$`AREA ACUMULADA EM 1 ANO`[i] <- sum(dataset$AREA[i-c(0:11)])
+    }
+dataset$`AREA ACUMULADA EM 6 MESES` <- NA
+for(i in 6:nrow(dataset)){
+  dataset$`AREA ACUMULADA EM 6 MESES`[i] <- sum(dataset$AREA[i-c(0:5)])
+  }
 
-
-ggplot(dataset)+
-  geom_line(aes(x=MES_ANO,y=AREA_ACUM_1ano))+
+ggplotly(ggplot(dataset)+
+  geom_line(aes(x=MES.ANO,y=`AREA ACUMULADA EM 1 ANO`, linetype = 'Acumulado em 1 ano'))+
+  geom_line(aes(x=MES.ANO,y=`AREA ACUMULADA EM 6 MESES`, linetype = 'Acumulado em 6 meses'))+
   scale_x_date(expand = c(0.05,0.05), breaks = '6 months',
                date_labels = '%b %Y', minor_breaks = NULL)+
+  scale_linetype_manual(name = NULL ,values = c('solid','dashed'))+
+  xlab(NULL)+
+  ylab('Área desmatada (km²)')+
+  ggtitle('Área desmatada na Amazônia Legal - Acumulado anual e semestral')+
   theme_light()+
-  theme(axis.text.x = element_text(angle=45, hjust = 1))
+  theme(axis.text.x = element_text(angle=45, hjust = 1),
+        plot.title = element_text(hjust=0.5))) %>%
+  layout(legend = list(orientation = 'h', bgcolor ='#FFFFFF00'))
 
+dataset$`DIFERENCA - 1 ANO` <- NA
+for(i in 24:nrow(dataset)){
+  dataset$`DIFERENCA - 1 ANO`[i] <- ((dataset$`AREA ACUMULADA EM 1 ANO`[i]/dataset$`AREA ACUMULADA EM 1 ANO`[i-12])-1)
+}
+
+dataset$`DIFERENCA - 6 MESES` <- NA
+for(i in 24:nrow(dataset)){
+  dataset$`DIFERENCA - 6 MESES`[i] <- ((dataset$`AREA ACUMULADA EM 6 MESES`[i]/dataset$`AREA ACUMULADA EM 6 MESES`[i-12])-1)
+}
+
+ggplotly(ggplot(dataset)+
+  geom_line(aes(x=MES.ANO,y=`DIFERENCA - 1 ANO`, linetype = 'Acumulado em 1 ano'))+
+  geom_line(aes(x=MES.ANO,y=`DIFERENCA - 6 MESES`, linetype = 'Acumulado em 6 meses'))+
+  geom_hline(yintercept = 0, color = 'red')+
+  scale_x_date(expand = c(0.05,0.05), breaks = '6 months',
+               date_labels = '%b %Y', minor_breaks = NULL)+
+  scale_y_continuous(expand = c(0,0.4), labels = scales::percent)+
+  scale_linetype_manual(name = NULL ,values = c('solid','dashed'))+
+  xlab(NULL)+
+  ggtitle(label = 'Desmatamento na Amazonônia Legal')+
+  ylab('Diferença entre um período e o mesmo período do ano anterior')+
+  theme_light()+
+  theme(axis.text.x = element_text(angle=45, hjust = 1)))%>%
+  layout(legend = list(orientation = 'h', bgcolor ='#FFFFFF00'))
 
 library(xlsx)
 write.xlsx(dataset,'dataset.xlsx')
+
